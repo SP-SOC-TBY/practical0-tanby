c020836c4898191024c420a0f8a316d352c3dd71
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2021-10-21 13:04:43 +0800

    Q1 done

--- practical0.js
+++ practical0.js
@@ -4,6 +4,42 @@
 let a = 96;
 console.log(a)
 
+// const - constant. will not change
+const n = 15;
+console.log(bigBus(n))
+
+// function with name bigBus and parameter abc
+function bigBus(abc){
+    // create an array as output
+    let output = []
+    // loop from 1 to abc
+    for(let i=1; i<=abc; i++){
+        // each time check if multiples of 3, 5, 15
+        //  if number is multiple of 15 then "BigBus"
+        // 15 goes first because it is a multiple of 3 and 5.
+        if(i%15 == 0){
+            output.push("BigBus")
+        }
+        //  if number is multiple of 3 then "Big"
+        else if(i%3 == 0){
+            output.push("Big")
+        }
+        //  if number is multiple of 5 then "Bus"
+        else if(i%5 == 0){
+            output.push("Bus")
+        }
+        // else do nothing, just use the number
+        else{
+            output.push(String(i))
+            //output.push(i.toString())
+            //output.push(i+"")
+        }
+        
+    }
+    // return the output
+    return output
+}
+
 // Q2
 
 // Q3
