3675a905cd320c4c1dc0b68054994260cd8551f0
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2021-10-28 11:48:10 +0800

    added error handling in fs read

---
+++ my-node-proj/chap1-4.js
@@ -0,0 +1,17 @@
+// include this module fs. so that i can read/write to the filesystem. 
+// with great power comes great responsibility - Spiderman uncle
+const fs = require('fs')
+
+fs.readFile(
+    "file.txt", //-- location of file to read
+    // we using arrow function 
+    (err, data) => { //-- callback when file read, includes error and the data read
+        // handle error
+        if(err !== null){ // got error
+            // print error out so that i can read and troubleshoot
+            console.log(err)
+        }else{ // no error
+            // print out data from file
+            console.log(data)
+        }
+})
